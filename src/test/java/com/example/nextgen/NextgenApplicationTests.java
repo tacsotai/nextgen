fb59a4deb1fb1367a2096b4f556e76e1e4df15ad
Maruyama Takeshi <maruyama-ta@iij.ad.jp>
2024-07-03 10:50:30 +0900

    Init Project #1

---
+++ src/test/java/com/example/nextgen/NextgenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextgenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
