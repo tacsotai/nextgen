6e80c54aa7d54a526da655cfd8cd1ccf35c1a726
DS\maruyama-ta <maruyama-ta@iij.ad.jp>
2024-07-23 12:12:01 +0900

    #35 Auto Format

--- src/main/java/com/example/nextgen/NextgenApplication.java
+++ src/main/java/com/example/nextgen/NextgenApplication.java
@@ -8,23 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @SpringBootApplication
 @RestController
-public class NextgenApplication  extends SpringBootServletInitializer {
+public class NextgenApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextgenApplication.class, args);
 	}
 
-    @Override
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(NextgenApplication.class);
 	}
-	
+
 	@GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
-    }
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 
 }
