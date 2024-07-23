ab8b2ebadb3eb7575d37a344254ced7bd02b41d9
DS\maruyama-ta <maruyama-ta@iij.ad.jp>
2024-07-23 12:11:06 +0900

    Not run spring boot war on tomcat #35

--- src/main/java/com/example/nextgen/NextgenApplication.java
+++ src/main/java/com/example/nextgen/NextgenApplication.java
@@ -2,17 +2,26 @@ package com.example.nextgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
-public class NextgenApplication {
+public class NextgenApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NextgenApplication.class, args);
 	}
+
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NextgenApplication.class);
+	}
+	
 	@GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
